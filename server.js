6c20a423c695ca9d2fc29f3207ae49209250898b
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-08-12 11:29:31 -0300

    some new modifications on server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+// const express = require('express');
+// const favicon = require('express-favicon');
+// const path = require('path');
+// const port = process.env.PORT || 8080;
+// const app = express();
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// // the __dirname is the current directory from where the script is running
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+// app.listen(port);
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -5,14 +20,12 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// API calls
-
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'dist')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
