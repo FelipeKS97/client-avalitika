cca6f8b631080ae08e54d12762205ca90e0cc236
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-08-12 16:03:10 -0300

    some new modifications on package.json and server.js(Heroku Deployment Tests - 7)

--- config/axios.js
+++ config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //axios.defaults.baseURL = 'http://localhost:3333/v1';
 
-const BASE_URL = 'http://server-avalitika.herokuapp.com/v1'
+const BASE_URL = 'https://server-avalitika.herokuapp.com/v1'
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL
