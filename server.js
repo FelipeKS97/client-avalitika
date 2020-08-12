007de58bdacc1f7ccc0a3428ded6357f58a297f2
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-08-12 14:41:08 -0300

    some new modifications on package.json and server.js(Heroku Deployment Tests - 3)

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    // res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(__dirname);
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
