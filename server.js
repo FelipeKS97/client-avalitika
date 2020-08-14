558a456bebc5622e5e71353931e632a6b013fc91
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-08-14 18:38:58 -0300

    changing configs of server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'dist')));
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'public/index.html'));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
