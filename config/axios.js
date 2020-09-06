a83baca3634d391bc91c9539be47c4c9bfd7bc54
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-09-06 19:28:39 -0300

    Changing deployment variable config

--- config/axios.js
+++ config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const LOCAL_API = 'http://localhost:3333/v1'
+const TEST_API = 'https://server-avalitika.herokuapp.com/v1' // Substitute this for your test api.
 
-export const BASE_URL = process.env.API_URL ? process.env.API_URL : LOCAL_API
+export const BASE_URL = process.env.API_URL ? process.env.API_URL : TEST_API
 export const axiosInstance = axios.create({
     baseURL: BASE_URL
 });
